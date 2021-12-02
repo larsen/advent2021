03fe6db4067d86eb4155df4b606bbd4a79466874
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2021-12-02 23:34:07 +0100

    OOP variation

--- day2.lisp
+++ day2.lisp
@@ -1,5 +1,38 @@
 (in-package #:advent2021)
 
+(defclass submarine ()
+  ((horizontal-position :initform 0 :accessor hpos)
+   (depth :initform 0 :accessor depth)
+   (aim :initform 0 :accessor aim)))
+
+(defgeneric forward (entity n))
+(defmethod forward ((s submarine) n)
+  (incf (hpos s) n)
+  (incf (depth s) (* (aim s) n)))
+
+(defgeneric down (entity n))
+(defmethod down ((s submarine) n)
+  (incf (aim s) n))
+
+(defgeneric up (entity n))
+(defmethod up ((s submarine) n)
+  (decf (aim s) n))
+
+(defun find-function-in-package (function-name package)
+  "Given a string FUNCTION-NAME and a PACKAGE, returns the function
+object corresponding to that function in the package. If the function
+does not exist it generates an error."
+  (symbol-function (find-symbol (string-upcase function-name) package)))
+
+(defgeneric execute-instructions (entity instructions))
+(defmethod execute-instructions ((s submarine) instructions)
+  (loop for instruction in instructions
+        for cmd = (first instruction)
+        for param = (second instruction)
+        do (funcall (find-function-in-package cmd :advent2021)
+                    s param)
+        finally (return s)))
+
 (defun read-course ()
   (loop for l in (uiop:read-file-lines
                   (asdf:system-relative-pathname 'advent2021 "inputs/day2"))
@@ -8,26 +41,13 @@
                   (list command (parse-integer param)))))
 
 (defun day2/solution1 ()
-  (loop with depth = 0
-        with horizontal-position = 0
-        for cmd in (read-course)
-        do (cond
-             ((string= "forward" (first cmd)) (incf horizontal-position (second cmd)))
-             ((string= "down" (first cmd)) (incf depth (second cmd)))
-             ((string= "up" (first cmd)) (decf depth (second cmd))))
-        finally (return (* horizontal-position
-                           depth))))
+  (let ((subm (make-instance 'submarine)))
+    (execute-instructions subm (read-course))
+    (* (hpos subm)
+       (aim subm))))
 
 (defun day2/solution2 ()
-  (loop with depth = 0
-        with horizontal-position = 0
-        with aim = 0
-        for cmd in (read-course)
-        do (cond
-             ((string= "forward" (first cmd))
-              (progn (incf horizontal-position (second cmd))
-                     (incf depth (* aim (second cmd)))))
-             ((string= "down" (first cmd)) (incf aim (second cmd)))
-             ((string= "up" (first cmd)) (decf aim (second cmd))))
-        finally (return (* horizontal-position
-                           depth))))
+  (let ((subm (make-instance 'submarine)))
+    (execute-instructions subm (read-course))
+    (* (hpos subm)
+       (depth subm))))
